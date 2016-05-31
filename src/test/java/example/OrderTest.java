4a33168d2157d710ce0c7ceebf16fcd492b545db
titian2046 <titian@21cn.com>
2016-05-31 14:51:25 +0800

    first test

--- src/test/java/example/OrderTest.java
+++ src/test/java/example/OrderTest.java
@@ -3,6 +3,7 @@ package example;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by titian on 2016/5/31.
@@ -12,6 +13,15 @@ public class OrderTest {
     public void testList() throws Exception {
         Order ord = new Order();
         assertEquals("food 1 21",ord.listMenu() [0]);
+        assertEquals("food 2 22",ord.listMenu() [1]);
+
+    }
+    @Test
+    public void testOrder()
+    {
+        Order ord = new Order();
+        assertEquals(true,ord.select(1));
+        assertEquals(1,ord.getUserOrder());
     }
 
 }

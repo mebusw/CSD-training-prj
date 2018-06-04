66c9cbaef09c3e6c8772e9746d67224dfcafacf3
OwenTse <xiejw@yahoo.com>
2018-06-04 14:55:57 +0800

    支持按地址搜索

--- src/main/java/conference/ConferenceSearch.java
+++ src/main/java/conference/ConferenceSearch.java
@@ -18,10 +18,11 @@ public class ConferenceSearch {
     public List<Room> searchAndSortByPrice(String addr, String status) {
         List<Room> result = new ArrayList<Room>();
         List<Room> rooms = RoomDB.getRooms();
-        for (int i = 0; i < rooms.size(); i++)
-            if (addr.equals(rooms.get(i).getAddr()) && status.equals(rooms.get(i).getStatus())) {
-                result.add(rooms.get(i));
-            }
+        for (int i = 0; i < rooms.size(); i++) {
+        if (addr.equals(rooms.get(i).getAddr()) && status.equals(rooms.get(i).getStatus())) {
+            result.add(rooms.get(i));
+        }
+        }
         return  result;
 
     }

1654b86590c46c552dd548cc1dd5c757cdd2ff39
OwenTse <xiejw@yahoo.com>
2018-06-04 14:53:57 +0800

    支持按地址搜索

--- src/test/conference/ConferenceTest.java
+++ src/test/conference/ConferenceTest.java
@@ -18,13 +18,12 @@ public class ConferenceTest {
         Assert.assertEquals("003",rooms.get(0).getRoomid());
     }
     @Test
-    public void testSubscribeRoom(){
+    public void testsearchAndSortByPriceRoom(){
         ConferenceSearch search = new ConferenceSearch();
         List<Room>  rooms = search.searchAndSortByPrice("杭州","free");
         System.out.println(rooms);
         Assert.assertTrue(rooms.size()>0);
         Assert.assertEquals("杭州",rooms.get(0).getAddr());
         Assert.assertEquals("free",rooms.get(0).getStatus());
-
     }
 }

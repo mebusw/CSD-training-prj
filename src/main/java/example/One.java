aae8f33f9db80433f83c554481fad264e73db7ce
Arthur Liang <1000874+arthurliang@users.noreply.github.com>
2018-06-04 14:51:33 +0800

    modify mainf function for end-end usage

--- src/main/java/example/One.java
+++ src/main/java/example/One.java
@@ -3,39 +3,23 @@ package example;
 import java.io.*;
 
 public class One {
-    String message = "foo";
-
-    public String foo() {
-        return message;
-    }
-
-    public void uncoveredMethod() {
-        System.out.println(foo());
-    }
 
     public static void main (String[] args)  throws IOException {
-        if (args[0].equals("consume")) {
-            BufferedReader in = null;
-            try {
-                in = new BufferedReader(new InputStreamReader(System.in));
-                String line;
-                while ((line = in.readLine()) != null) {
-                    System.out.println(line);
-                }
-            }
-            catch (IOException e) {
-                throw e;
-            }
-            finally {
-                if (in != null) {
-                    in.close();
-                }
-            }
-        } else {
-            for (String s: args) {
-                System.out.println(s);
-            }
+        if (args == null) {
+            System.out.println("Args should be not null.");
+            return;
         }
 
+        String customer = "user";
+
+        if (args[0].equals(customer)) {
+            if (args.length == 1) {
+                System.out.println("user need other args: search or reserve.");
+                return;
+            }
+
+            System.out.println("Currently, there is not available meeting rooms.");
+            return;
+        }
     }
 }
--- src/test/java/example/OneTest.java
+++ src/test/java/example/OneTest.java
@@ -1,18 +1,81 @@
 package example;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Test;
 
+import java.io.*;
+
 public class OneTest {
-  @Test
-  public void testFoo() throws Exception {
-    One one = new One();
-    assertEquals("foo", one.foo());
-  }
 
   @Test
-  public void testSeconde() {
+  public void testMainWithNullArgs() throws IOException {
+    // change System for test
+    final PrintStream original = System.out;
+    final PrintStream mockPrintStream = mock(PrintStream.class);
+    System.setOut(mockPrintStream);
 
-    assertEquals(1, 1);
+    // arrange
+    String[] args = null;
+    String expectedOut = "Args should be not null.";
+
+
+    // act
+    One.main(args);
+
+    // assert
+    verify(mockPrintStream).println(expectedOut);
+
+    // recover System
+    System.setOut(original);
   }
+
+
+  @Test
+  public void testMainWithArgs0isUser_Without_EnoughArgsN() throws IOException {
+    // change System for test
+    final PrintStream original = System.out;
+    final PrintStream mockPrintStream = mock(PrintStream.class);
+    System.setOut(mockPrintStream);
+
+    // arrange
+    String[] args = {"user"};
+    String expectedOut = "user need other args: search or reserve.";
+
+    // act
+    One.main(args);
+
+    // assert
+    verify(mockPrintStream).println(expectedOut);
+
+    // recover System
+    System.setOut(original);
+  }
+
+
+  @Test
+  public void testMainWithArgs0isUser_With_Search_Without_Available_Rooms() throws IOException {
+    // change System for test
+    final PrintStream original = System.out;
+    final PrintStream mockPrintStream = mock(PrintStream.class);
+    System.setOut(mockPrintStream);
+
+    // arrange
+    String[] args = {"user", "search"};
+    String expectedOut = "Currently, there is not available meeting rooms.";
+
+    // act
+    One.main(args);
+
+    // assert
+    verify(mockPrintStream).println(expectedOut);
+
+    // recover System
+    System.setOut(original);
+  }
+
+
+
 }

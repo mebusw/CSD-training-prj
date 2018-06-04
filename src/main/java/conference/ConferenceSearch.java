d33db361a3b89506e2b1a4f33f17976e08a0b21e
OwenTse <xiejw@yahoo.com>
2018-06-04 15:03:01 +0800

    Conference 和 测试类重构

--- src/main/java/conference/ConferenceSearch.java
+++ src/main/java/conference/ConferenceSearch.java
@@ -7,10 +7,16 @@ public class ConferenceSearch {
     public List<Room> search(String addr) {
         List<Room> result = new ArrayList<Room>();
         List<Room> rooms = RoomDB.getRooms();
-        for (int i = 0; i < rooms.size(); i++)
-            if (addr.equals(rooms.get(i).getAddr())) {
-                result.add(rooms.get(i));
+        if (null == addr){
+            return rooms;
+        }
+        Room room;
+        for (int i = 0; i < rooms.size(); i++) {
+            room = rooms.get(i);
+            if (addr.equals(room.getAddr())) {
+                result.add(room);
             }
+        }
         return result;
     }
 
@@ -18,12 +24,14 @@ public class ConferenceSearch {
     public List<Room> searchAndSortByPrice(String addr, String status) {
         List<Room> result = new ArrayList<Room>();
         List<Room> rooms = RoomDB.getRooms();
+        Room room;
         for (int i = 0; i < rooms.size(); i++) {
-        if (addr.equals(rooms.get(i).getAddr()) && status.equals(rooms.get(i).getStatus())) {
-            result.add(rooms.get(i));
+            room = rooms.get(i);
+            if (addr.equals(room.getAddr()) && status.equals(room.getStatus())) {
+                result.add(room);
+            }
         }
-        }
-        return  result;
+        return result;
 
     }
 }
--- src/test/conference/ConferenceTest.java
+++ src/test/conference/ConferenceTest.java
@@ -1,21 +1,36 @@
 package conference;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
 public class ConferenceTest {
 
+
+    private ConferenceSearch search;
+
+    @Before
+    public void setUp() throws Exception {
+        search = new ConferenceSearch();
+    }
+
     @Test
     public void testSearchConferenRoom() {
-        ConferenceSearch search = new ConferenceSearch();
         List<Room>  rooms = search.search("杭州");
         Assert.assertEquals("001",rooms.get(0).getRoomid());
         rooms = search.search("上海");
         Assert.assertEquals("002",rooms.get(0).getRoomid());
         rooms = search.search("北京");
         Assert.assertEquals("003",rooms.get(0).getRoomid());
+    }
+    @Test
+    public void testSearchConferenRoomAddrIsNull() {
+        ConferenceSearch search = new ConferenceSearch();
+        //地址为空返回所有酒店
+        List<Room>  rooms = search.search(null);
+        Assert.assertTrue(rooms.size()>0);
     }
     @Test
     public void testsearchAndSortByPriceRoom(){

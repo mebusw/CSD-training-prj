158c58eef6231fb2102c49e4ac34ef4fc76a08d1
OwenTse <xiejw@yahoo.com>
2018-06-08 14:25:09 +0800

    prime 重构测试类为参数化单元测试

--- src/test/java/primeFactor/PrimeFactorTest.java
+++ src/test/java/primeFactor/PrimeFactorTest.java
@@ -3,14 +3,35 @@ package primeFactor;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
+@RunWith(Parameterized.class)
 public class PrimeFactorTest {
+
+    private List result;
+    private int number;
+
+    public PrimeFactorTest(List result, int number){
+        this.result = result;
+        this.number = number;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> getAllCases() {
+        return Arrays.asList(new Object[][]{
+                {Arrays.asList(2),2},
+                {Arrays.asList(3),3}
+                });
+    }
 
     @Test
     public  void testPrimeFactor(){
-        Assert.assertEquals(Arrays.asList(2),PrimeFactor.factor(2));
-        Assert.assertEquals(Arrays.asList(3),PrimeFactor.factor(3));
+//        Assert.assertEquals(Arrays.asList(2),PrimeFactor.factor(2));
+        Assert.assertEquals(this.result,PrimeFactor.factor(this.number));
+//        Assert.assertEquals(Arrays.asList(3),PrimeFactor.factor(3));
     }
 }

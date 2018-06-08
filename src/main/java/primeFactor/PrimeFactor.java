bf1413f3662873bd97d041eb47906d25f93b865f
OwenTse <xiejw@yahoo.com>
2018-06-08 09:22:02 +0800

    prime Test

---
+++ src/main/java/primeFactor/PrimeFactor.java
@@ -0,0 +1,12 @@
+package primeFactor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactor {
+
+    public static List factor(int number) {
+        List result = new ArrayList();
+        return result;
+    }
+}
---
+++ src/test/primeFactor/PrimeFactorTest.java
@@ -0,0 +1,16 @@
+package primeFactor;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class PrimeFactorTest {
+
+    @Test
+    public  void testPrimeFactor(){
+        Assert.assertEquals(Arrays.asList(2),PrimeFactor.factor(2));
+
+    }
+}

945f8500ab54f7f6786553a471a647a9eacffef5
OwenTse <xiejw@yahoo.com>
2018-06-10 20:12:55 +0800

    Add Groovy Spock TestCase

--- src/main/java/primeFactor/PrimeFactor.java
+++ src/main/java/primeFactor/PrimeFactor.java
@@ -5,9 +5,8 @@ import java.util.List;
 
 public class PrimeFactor {
 
-    public static List factor(int number) {
-        List result = new ArrayList();
-
+    public static List<Integer> factor(int number) {
+        List<Integer> result = new ArrayList<Integer>();
         for (int i=2;i<=number;i++){
             while (number % i == 0) {
                 result.add(i);
---
+++ src/test/groovy/ConferenceSpockTest.groovy
@@ -0,0 +1,19 @@
+package groovy
+
+import conference.ConferenceSearch
+import conference.Room
+
+class ConferenceSpockTest extends spock.lang.Specification {
+    def "按地点搜索会议室"(){
+        given:"初始化搜索类"
+            ConferenceSearch conferenceSearch = new ConferenceSearch()
+        when:"进行搜索"
+            List<Room> result =  conferenceSearch.search("杭州")
+        then:"判断搜索结果"
+            for (Room room : result){
+                println(room)
+                assert room.getAddr() == "杭州"
+            }
+
+    }
+}

a90b75955dd05dbf6f42c2757befbdd37e5fab75
OwenTse <xiejw@yahoo.com>
2018-06-14 22:41:43 +0800

    使用spock简洁的测试方法

--- src/test/groovy/primeFactor/PrimeFactorSpockTest.groovy
+++ src/test/groovy/primeFactor/PrimeFactorSpockTest.groovy
@@ -9,15 +9,14 @@ class PrimeFactorSpockTest extends Specification {
         PrimeFactor.factor(number as int) == result as List
         where:""
         number | result
-        2|[2]
-        3|[3]
-        4|[2,2]
-        5|[5]
-        6|[2,3]
-        7|[7]
-        8|[2,2,2]
-        9|[3,3]
-        2*5*11|[2,5,11]
-
+        2 | [2]
+        3 | [3]
+        4 | [2,2]
+        5 | [5]
+        6 | [2,3]
+        7 | [7]
+        8 | [2,2,2]
+        9 | [3,3]
+        2*5*11 | [2,5,11]
     }
 }

d1fba78140429eef221a2fe0d32091fcbd2e5bb9
OwenTse <xiejw@yahoo.com>
2018-06-18 13:24:47 +0800

    增加参数化测试方式

--- src/test/groovy/fizzBuzz/FizzBuzzSpockTest.groovy
+++ src/test/groovy/fizzBuzz/FizzBuzzSpockTest.groovy
@@ -1,6 +1,7 @@
 package groovy.fizzBuzz
 
 import fizzBuzz.FizzBuzz
+import spock.lang.Unroll
 
 class FizzBuzzSpockTest extends spock.lang.Specification{
     def "测试数字1"(){
@@ -44,6 +45,29 @@ class FizzBuzzSpockTest extends spock.lang.Specification{
     def "测试数字1到15"(){
         expect:""
         FizzBuzz.showOutput(1,15)=="1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n"
+    }
+
+    @Unroll
+    def "测试单个数字，采用参数化测试方法"(){
+        expect:"多场景"
+        FizzBuzz.answer(number as int) == result as String
+        where:
+        number | result
+        1  | "1"
+        2  | "2"
+        3  | "Fizz"
+        4  | "4"
+        5  | "Buzz"
+        6  | "Fizz"
+        7  | "7"
+        8  | "8"
+        9  | "Fizz"
+        10 | "Buzz"
+        11 | "11"
+        12 | "Fizz"
+        15 | "FizzBuzz"
+
+
     }
 
 }
